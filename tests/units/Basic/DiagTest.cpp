2db5d69dd2ecdc07af15f69b9e3514aae9a97e10
McCorney Severin <mccorney.severin@gmail.com>
2021-07-15 20:10:11 -0700

    Trivial.

--- tests/units/Basic/DiagTest.cpp
+++ tests/units/Basic/DiagTest.cpp
@@ -1,10 +1,17 @@
 #include "stone/Basic/DiagnosticEngine.h"
+#include "stone/Basic/Basic.h"
+
 #include "gtest/gtest.h"
 
 using namespace stone;
 
 class DiagTest : public ::testing::Test {
+	Basic basic; 
+
 public:
 };
 
-TEST_F(DiagTest, ErrorTest) {}
+TEST_F(DiagTest, ErrorTest) {
+
+	auto diagEngine = basic.GetDiagEngine(); 
+}
