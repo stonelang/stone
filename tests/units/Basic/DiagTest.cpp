8ef0621e9dadb6ac514fb8119fa2284f547f4c56
McCorney Severin <mccorney.severin@gmail.com>
2021-07-21 10:59:27 -0700

    Updating Unit Tests

--- tests/units/Basic/DiagTest.cpp
+++ tests/units/Basic/DiagTest.cpp
@@ -3,7 +3,7 @@
 #include "stone/Basic/DiagnosticEngine.h"
 #include "stone/Basic/SynDiagnostic.h"
 #include "stone/Basic/TextDiagnosticListener.h"
-#include "stone/Syntax/SyntaxDiagArgument.h"
+#include "stone/Syntax/SyntaxDiagnosticArgument.h"
 
 using stone::diag::DeclArgument;
 
@@ -18,18 +18,12 @@ public:
 public:
 };
 
-TEST_F(DiagTest, BasicTest) {
+TEST_F(DiagTest, DiagnoseSyntax) {
 
   basic.GetDiagOptions().useColor = true;
 
   TextDiagnosticListener textListener;
   basic.GetDiagEngine().AddListener(textListener);
-
-  // basic.GetDiagEngine().Diagnose(diag::err_no_compile_args)
-  //     << "test with no 'SrcLoc'";
-
-  // basic.GetDiagEngine().Diagnose(SrcLoc(), diag::err_no_compile_args)
-  //     << "test with blank 'SrcLoc'";
 
   basic.GetDiagEngine()
       .Diagnose(SrcLoc(), diag::note_prev_decl_def, diag::DeclArgument(nullptr))
