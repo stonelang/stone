27607c7bacf3558a823c5109c3b89f057682e868
stonelang <stonelang@yahoo.com>
2022-04-17 09:44:16 -0700

    Updating Compile

---
+++ include/stone/Compile/ModuleLoader.h
@@ -0,0 +1,11 @@
+#ifndef STONE_COMPILE_MODULELOADER_H
+#define STONE_COMPILE_MODULELOADER_H
+
+namespace stone {
+class ModuleLoader final {
+public:
+  ModuleLoader();
+  ~ModuleLoader();
+};
+} // namespace stone
+#endif
---
+++ include/stone/Compile/ModuleSerialization.h
@@ -0,0 +1,15 @@
+#ifndef STONE_COMPILE_MODULESERIALIZATION_H
+#define STONE_COMPILE_MODULESERIALIZATION_H
+
+
+namespace stone {
+
+//TODO: protocolbuf
+class ModuleSerialization final {
+public:
+  ModuleSerialization();
+  ~ModuleSerialization();
+};
+
+} // namespace stone
+#endif
