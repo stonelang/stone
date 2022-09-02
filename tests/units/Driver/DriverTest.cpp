7acff19899913e083e04f706f465bb0cb092ebfc
McCorney Severin <severin@severinmbpro.lan>
2022-09-01 21:21:23 -0700

    Updatig Driver

--- tests/units/Driver/DriverTest.cpp
+++ tests/units/Driver/DriverTest.cpp
@@ -1,8 +1,8 @@
-#include "stone/Driver/Driver.h"
+#include "stone/Drive/Driver.h"
 #include "stone/Basic/LangOptions.h"
 #include "stone/Basic/SrcMgr.h"
-#include "stone/Driver/CompilationJob.h"
-#include "stone/Driver/Phase.h"
+#include "stone/Drive/CompilationJob.h"
+#include "stone/Drive/Phase.h"
 #include "stone/LangContext.h"
 #include "gtest/gtest.h"
 
