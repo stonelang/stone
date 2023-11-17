a7b14b03f540617f54b228c6e49a10db55e5fe7c
McCorney Severin <severin@severinmbpro.lan>
2023-11-16 22:15:40 -0800

    Clean compile

--- compiler/include/stone/Compile/Compile.h
+++ compiler/include/stone/Compile/Compile.h
@@ -2,32 +2,9 @@
 #define STONE_COMPILE_COMPILE_H
 
 #include "llvm/ADT/ArrayRef.h"
-#include "llvm/ADT/StringRef.h"
 
 namespace stone {
-
 class CompilerListener;
-
-// class CompilerListener {
-// public:
-//   CompilerListener() = default;
-//   virtual ~CompilerListener() = default;
-
-// public:
-//   virtual void CompletedCommandLineParsing(CompilerInvocation &invocation) {}
-//   virtual void CompletedInitialization(Compiler &compiler) {}
-//   virtual void CompletedBuildingTasks(Compiler &compiler) {}
-
-//   virtual void ExecutingCompilerTasks(Compiler &compiler, CompilerTask *task)
-//   {} virtual void FinishedCompilerTasks(Compiler &compiler, CompilerTask
-//   *task) {}
-
-//   virtual void CompletedSyntaxAnalysis(Compiler &compiler) {}
-//   virtual void CompletedSemanticAnalysis(Compiler &compiler) {}
-//   virtual void CompletedCodeGeneration(Compiler &compiler) {}
-//   virtual void CompletedRunningTasks(Compiler &compiler) {}
-// };
-
 int Compile(llvm::ArrayRef<const char *> args, const char *arg0, void *mainAddr,
             CompilerListener *listener = nullptr);
 
--- compiler/lib/Compile/ModuleDependencies.cpp
+++
@@ -1,1 +0,0 @@
-#include "stone/Compile/ModuleDependencies.h"
