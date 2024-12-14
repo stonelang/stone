fa66cacbc043fe835f312802299d1ac686f945ed
McCorney Severin <severin@MacBookPro.lan>
2024-12-13 23:55:28 -0800

    Updating Driver

--- compiler/include/stone/Driver/CompilationEntity.h
+++ compiler/include/stone/Driver/CompilationEntity.h
@@ -35,15 +35,59 @@ enum class CompilationEntityKind : uint8_t {
 };
 
 /// A list of all job construction inputs
-using CompilationEntityList = llvm::ArrayRef<const CompilationEntity *>;
+using CompilationEntityList = llvm::SmallVector<const CompilationEntity *, 3>;
 
 constexpr size_t CompilationEntityAlignInBits = 8;
 
 class alignas(1 << CompilationEntityAlignInBits) CompilationEntity
     : public DriverAllocation<CompilationEntity> {
 
-};
+  CompilationEntityKind kind;
+  FileType fileType;
+  CompilationEntityList allInputs;
 
+public:
+  using size_type = CompilationEntityList::size_type;
+  using input_iterator = CompilationEntityList::iterator;
+  using input_const_iterator = CompilationEntityList::const_iterator;
+  using input_range = llvm::iterator_range<input_iterator>;
+  using input_const_range = llvm::iterator_range<input_const_iterator>;
+
+  public:
+  CompilationEntity(CompilationEntityKind kind, FileType fileType)
+      : CompilationEntity(kind, CompilationEntityList(), fileType) {}
+
+  CompilationEntity(CompilationEntityKind kind, CompilationEntity *input, FileType fileType)
+      : CompilationEntity(kind, CompilationEntityList({input}), fileType) {}
+
+  CompilationEntity(CompilationEntityKind kind, CompilationEntity *input)
+      : CompilationEntity(kind, CompilationEntityList({input}), input->GetFileType()) {}
+
+  CompilationEntity(CompilationEntityKind kind, const CompilationEntityList &inputs, FileType fileType)
+      : kind(kind), fileType(fileType), allInputs(inputs) {}
+
+
+public:
+  CompilationEntityKind GetKind() const { return kind; }
+  FileType GetFileType() const { return fileType; }
+  bool HasFileType() { return fileType != FileType::None; }
+
+  CompilationEntityList &GetInputs() { return allInputs; }
+  const CompilationEntityList &GetInputs() const { return allInputs; }
+  void AddInput(CompilationEntity *input) { allInputs.push_back(input); }
+
+public:
+  size_type size() const { return allInputs.size(); }
+  input_iterator input_begin() { return allInputs.begin(); }
+  input_iterator input_end() { return allInputs.end(); }
+  input_range inputs() { return input_range(input_begin(), input_end()); }
+  input_const_iterator input_begin() const { return allInputs.begin(); }
+  input_const_iterator input_end() const { return allInputs.end(); }
+  input_const_range inputs() const {
+    return input_const_range(input_begin(), input_end());
+  }
+
+};
 
 } // namespace stone
 
