abba7eacf255c02e75c6bfa7dfd54605cc300bbf
McCorney Severin <severin@MacBookPro.lan>
2025-01-17 10:14:18 -0800

    Updating Property

--- include/stone/AST/Property.h
+++ include/stone/AST/Property.h
@@ -40,6 +40,24 @@ class alignas(1 << PropertyAlignInBits) Property
 
 public:
   Property(PropertyKind kind, SrcLoc loc) : kind(kind), loc(loc) {}
+
+public:
+  bool IsModifier() const;
+  bool IsAttribute() const;
+
+public:
+  bool IsStatic() const { return kind == PropertyKind::Static; }
+  bool IsExern() const { return kind == PropertyKind::Extern; }
+  bool IsPublic() const { return kind == PropertyKind::Public; }
+  bool IsProtected() const { return kind == PropertyKind::Protected; }
+  bool IsPrivate() const { return kind == PropertyKind::Private; }
+  bool IsConst() const { return kind == PropertyKind::Const; }
+  bool IsStone() const { return kind == PropertyKind::Const; }
+  bool IsPure() const { return kind == PropertyKind::Pure; }
+  bool IsVolaltile() const { return kind == PropertyKind::Volatile; }
+
+  bool IsDeprecated() const { return kind == PropertyKind::Deprecated; }
+  bool IsAligned() const { return kind == PropertyKind::Aligned; }
 };
 
 class Modifier : public Property {
@@ -50,10 +68,43 @@ public:
 class DeclModifier : public Modifier {
 public:
   DeclModifier(PropertyKind kind, SrcLoc loc) : Modifier(kind, loc) {}
+
+public:
+  bool IsStorage() const;
+  bool IsVisibility() const;
 };
+
+class StaticModifier : public DeclModifier {
+
+public:
+  StaticModifier(SrcLoc loc) : DeclModifier(PropertyKind::Static, loc) {}
+};
+
+class ExternModifier : public DeclModifier {
+public:
+  ExternModifier(SrcLoc loc) : DeclModifier(PropertyKind::Extern, loc) {}
+};
+class PublicModifier : public DeclModifier {
+public:
+  PublicModifier(SrcLoc loc) : DeclModifier(PropertyKind::Public, loc) {}
+};
+class ProtectedModifier : public DeclModifier {
+public:
+  ProtectedModifier(SrcLoc loc) : DeclModifier(PropertyKind::Protected, loc) {}
+};
+class PrivateModifier : public DeclModifier {
+public:
+  PrivateModifier(SrcLoc loc) : DeclModifier(PropertyKind::Private, loc) {}
+};
+
 class TypeModifier : public Modifier {
 public:
   TypeModifier(PropertyKind kind, SrcLoc loc) : Modifier(kind, loc) {}
+};
+
+class ConstModifier : public TypeModifier {
+public:
+  ConstModifier(SrcLoc loc) : TypeModifier(PropertyKind::Const, loc) {}
 };
 
 class Attribute : public Property {
@@ -66,9 +117,18 @@ public:
   DeclAttribute(PropertyKind kind, SrcLoc loc) : Attribute(kind, loc) {}
 };
 
+class DeprecatedAttribute : public DeclAttribute {
+public:
+  DeprecatedAttribute(SrcLoc loc) : DeclAttribute(PropertyKind::Deprecated, loc) {}
+};
+
 class TypeAttribute : public Attribute {
 public:
   TypeAttribute(PropertyKind kind, SrcLoc loc) : Attribute(kind, loc) {}
+};
+class AlignAttribute : public TypeAttribute {
+public:
+  AlignAttribute(SrcLoc loc) : TypeAttribute(PropertyKind::Aligned, loc) {}
 };
 
 } // namespace stone
