a56c4019e708100208c1b1e39665cada1ab10d60
McCorney Severin <severin@MacBookPro.lan>
2025-01-27 12:35:34 -0800

    Updating Decl

--- include/stone/AST/Module.h
+++ include/stone/AST/Module.h
@@ -2,6 +2,7 @@
 #define STONE_AST_MODULE_H
 
 #include "stone/AST/ASTContext.h"
+#include "stone/AST/ASTAllocation.h"
 #include "stone/AST/ASTScope.h"
 #include "stone/AST/ASTWalker.h"
 #include "stone/AST/Decl.h"
@@ -246,7 +247,7 @@ public:
 
 class ModuleDecl final : public DeclContext,
                          public TypeDecl,
-                         public MemoryAllocation<ModuleDecl> {
+                         public ASTAllocation<ModuleDecl> {
 
   // TODO: This is not yet implemented
   ModuleDecl *parent = nullptr;
